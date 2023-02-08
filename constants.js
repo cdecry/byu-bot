6271844227af9fdbfe948638d370d98541b6d84a
crystal / ue <mfihy1@gmail.com>
2023-02-08 14:25:48 -0500

    initial commit

---
+++ constants.js
@@ -0,0 +1,11 @@
+module.exports = Object.freeze({
+    ABOUT_TITLE: "hi, i'm byu~",
+    ABOUT_DESCRIPTION: "**byu-bot** is a miscellaneous discord bot originally created for the purpose of custom embedded auto responses!",
+    ABOUT_COLOR: 0xcae7f9,
+    ABOUT_AUTHOR: "byu",
+    ABOUT_AUTHOR_ICON: "https://i.pinimg.com/originals/32/ae/a1/32aea150742ae5fc9b6af2a727271900.png",
+    ABOUT_IMAGE: "https://cdn.discordapp.com/attachments/707639739888435301/731940350439981136/unknown.png",
+    ABOUT_THUMBNAIL: "https://cdn.discordapp.com/attachments/707639739888435301/1072947217385652244/image.png",
+    ABOUT_FOOTER: "use  ~help  to see some of byu's commands!",
+    ABOUT_FOOTER_ICON: "https://i.pinimg.com/originals/32/ae/a1/32aea150742ae5fc9b6af2a727271900.png",
+});
